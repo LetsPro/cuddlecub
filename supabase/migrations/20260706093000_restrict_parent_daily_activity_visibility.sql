cf368c4df995f0c03cffbbd8f0e2116173532a38
LetsPro <87424405+LetsPro@users.noreply.github.com>
2026-07-06 20:10:20 +0530

    Updated index.html

--- supabase/migrations/20260706093000_restrict_parent_daily_activity_visibility.sql
+++
@@ -1,8 +0,0 @@
-drop policy if exists daily_activity_logs_staff_parent_select on public.daily_activity_logs;
-create policy daily_activity_logs_staff_parent_select on public.daily_activity_logs
-for select to authenticated
-using (
-  public.is_school_admin(school_id)
-  or public.can_staff_access_student(student_id)
-  or (shared_with_parent and public.can_parent_access_student(student_id))
-);
